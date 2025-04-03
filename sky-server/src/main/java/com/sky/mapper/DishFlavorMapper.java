9ef7903bc3825673aded8e7b44ccb45bb7450841
Aible <xjx.plm@gmail.com>
2025-04-03 18:00:22 +0800

    新增菜品业务开发

--- sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishFlavorMapper.java
@@ -1,6 +1,8 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
 import com.sky.entity.DishFlavor;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -11,5 +13,6 @@ public interface DishFlavorMapper {
      * 批量插入口味数据
      * @param flavors
      */
+    @AutoFill(value = OperationType.INSERT)
     void insertBatch(List<DishFlavor> flavors);
 }
